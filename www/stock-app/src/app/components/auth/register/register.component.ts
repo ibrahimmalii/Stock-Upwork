e02de02d8a5dc11f5a685f68f77ca21f708af604
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-09-27 19:13:06 +0200

    add register form

--- www/stock-app/src/app/components/auth/register/register.component.ts
+++ www/stock-app/src/app/components/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder : FormBuilder) { }
+
+  form : FormGroup = new FormGroup({});
+  isLogged : Boolean = false;
+  isLoginSuccess = false;
+  isLoginError = false;
 
   ngOnInit(): void {
+    // Validate Login Form
+    this.form = this.formBuilder.group({
+      email : ['' , [Validators.email ,Validators.maxLength(255) , Validators.required] ],
+      password : ['' , [Validators.required , Validators.minLength(8) , Validators.maxLength(15)]],
+      password_confirmation : ['' , [Validators.required , Validators.minLength(8) , Validators.maxLength(15)]],
+    });
+
+  }
+
+  register(){
+    this.isLogged = true;
+
+    if(this.form.valid && this.form.controls.password == this.form.controls.password_confirmation){
+      this.isLoginSuccess = true;
+      this.isLoginError = false
+      console.log(this.form.value);
+    }else {
+      this.isLoginError = true;
+      this.isLoginSuccess = false;
+    }
+
+  }
+
+  hide(){
+    this.isLoginSuccess = false;
+    this.isLoginError = false;
   }
 
 }
