a5bb13653dbec08ada59d9aa439615a440b92214
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-09-27 02:09:58 +0200

    add validation to login form

--- www/stock-app/src/app/app.module.ts
+++ www/stock-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { SharedModule } from './components/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { SharedModule } from './components/shared/shared.module';
     AppRoutingModule,
     RouterModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- www/stock-app/src/app/components/auth/login/login.component.ts
+++ www/stock-app/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder : FormBuilder) { }
+
+  form : FormGroup = new FormGroup({});
+  isLogged : Boolean = false;
+  isLoginSuccess = false;
+  isLoginError = false;
 
   ngOnInit(): void {
+    // Validate Login Form
+    this.form = this.formBuilder.group({
+      email : ['' , [Validators.email ,Validators.maxLength(255) , Validators.required] ],
+      password : ['' , [Validators.required , Validators.minLength(8) , Validators.maxLength(15)]]
+    });
+
+  }
+
+  login(){
+    this.isLogged = true;
+
+    if(this.form.valid){
+      this.isLoginSuccess = true;
+      console.log(this.form.value);
+    };
+
+    this.isLoginError = true;
+
+    
   }
 
 }
