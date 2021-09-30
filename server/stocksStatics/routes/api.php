ac7bff548096bb0bcc37b834ccf0132bbeea07f8
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-10-01 01:57:26 +0200

    retreive the data from quickFS Api

--- server/stocksStatics/routes/api.php
+++ server/stocksStatics/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Client\Response;
+use App\Models\Test;
+use Illuminate\Support\Facades\Http;
+
+
 
 
 /*
@@ -24,3 +30,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login' ,[AuthController::class , 'login'] );
 Route::get('/logout' , [AuthController::class, 'logout']);
+
+
+$responseData = '';
+Route::get('/test', function(Request $request){
+    $response = Http::get('https://public-api.quickfs.net/v1/data/all-data/FB:US?api_key=4ed0f30c148834139f4bb3c4421341690f3d3c07');
+    $response->json();
+    $responseData = $response->json();
+    dd($responseData);
+});
