befd9d72618a972cd564f764a779597654fa1927
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-09-30 03:12:11 +0200

    add login request into login component

--- www/stock-app/src/app/components/auth/login/login.component.ts
+++ www/stock-app/src/app/components/auth/login/login.component.ts
@@ -11,41 +11,59 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router : Router, private activatedRoute : ActivatedRoute) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router, private activatedRoute: ActivatedRoute) { }
 
-  form : FormGroup = new FormGroup({});
-  isLogged : Boolean = false;
+  form: FormGroup = new FormGroup({});
+  isLogged: Boolean = false;
   isLoginSuccess = false;
   isLoginError = false;
-  code : string = '';
-  responseData : any;
+  code: string = '';
+  responseData: any;
+  logginData: any;
 
   ngOnInit(): void {
 
 
     // Validate Login Form
     this.form = this.formBuilder.group({
-      email : ['' , [Validators.email ,Validators.maxLength(255) , Validators.required] ],
-      password : ['' , [Validators.required , Validators.minLength(8) , Validators.maxLength(15)]]
+      email: ['', [Validators.email, Validators.maxLength(255), Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15)]]
     });
 
   }
 
-  login(){
+  login() {
     this.isLogged = true;
 
-    if(this.form.valid){
-      this.isLoginSuccess = true;
-      this.isLoginError = false
+    if (this.form.valid) {
       console.log(this.form.value);
-    }else {
+      this.http.post('http://localhost:8000/api/login', this.form.value).subscribe(res => {
+        this.logginData = res;
+        if (this.logginData.data != null) {
+          localStorage.token = this.logginData.data.access_token;
+          localStorage.user = JSON.stringify(this.logginData.data.user);
+          this.router.navigateByUrl('/exchanges');
+          this.isLoginSuccess = true;
+          this.isLoginError = false;
+        } else {
+          this.isLoginError = true;
+          this.isLoginSuccess = false;
+        }
+
+      }, (error) => {
+        this.isLoginError = true;
+        this.isLoginSuccess = false;
+      })
+
+
+    } else {
       this.isLoginError = true;
       this.isLoginSuccess = false;
     }
 
   }
 
-  hide(){
+  hide() {
     this.isLoginSuccess = false;
     this.isLoginError = false;
   }
